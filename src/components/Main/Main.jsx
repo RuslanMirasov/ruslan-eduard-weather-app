dcafa24b7ae0b8db04bfaf70d3b59dbe099cb4a6
Ruslan Mirasov <info@mirasov.dev>
2024-09-02 18:48:40 +0200

    add styles to header footer and main components and change favicon

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,11 @@
 import "./Main.css";
 
-const Main = ({ children }) => {
-  return <main className="main">{children}</main>;
+const Main = ({ isGoodWeather, children }) => {
+  return (
+    <main className={`main ${!isGoodWeather ? "main--bad" : ""}`}>
+      {children}
+    </main>
+  );
 };
 
 export default Main;
